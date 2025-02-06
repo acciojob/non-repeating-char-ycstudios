96d7a22d40b321f001ea44ff98f33f9981f851c8
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-06 13:08:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
+    let charCount = {};
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];  
+        if (charCount[char] === undefined) { 
+            charCount[char] = 1;
+        } else {
+            charCount[char]++;
+        }
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]] === 1) {
+            return str[i];
+        }
+    }
+
+    return null;}
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
